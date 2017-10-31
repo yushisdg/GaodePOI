be41fef7e65a7c93578c0b8e1bc3ed52114c20f0
yushisdg <fangqiwei2013@163.com>
2017-10-31 16:57:29 +0800

    tijiao

--- code/GaodePoiCrawler.py
+++ code/GaodePoiCrawler.py
@@ -11,6 +11,7 @@ pages=1;
 def getGaodePOI(key,rectangle,type,page,offset):
     poiUrl="http://restapi.amap.com/v3/place/polygon?polygon="+rectangle+"&types="+type+"&output=json&key="+key+"&page="+page+"&offset="+offset+"&extensions=all"
     res = requests.get(url=poiUrl).content;
+    afterRequest(key);
     total_json = json.loads(res);
     print(total_json);
     insert_list = [];
@@ -80,23 +81,7 @@ def getGaodePOI(key,rectangle,type,page,offset):
             photos = json.dumps(poi.get('photos'));
             sql = "INSERT INTO gaode_poi (id, tag, name, type, typecode, biz_type, address, location, distance, tel, postcode, website, email, pcode, pname, citycode, cityname, adcode, adname, entr_location, exit_location, navi_poiid, gridcode, alias, business_area, parking_type, indoor_map, cpid, floor, truefloor, groupbuy_num, discount_num, rating, cost, meal_ordering, seat_ordering, ticket_ordering, hotel_ordering, photos) VALUES ('" + id + "','" + tag + "','" + name + "','" + type + "','" + typecode + "','" + biz_type + "','" + address + "','" + location + "','" + distance + "','" + tel + "','" + postcode + "','" + website + "','" + email + "','" + pcode + "','" + pname + "','" + citycode + "','" + cityname + "','" + adcode + "','" + adname + "','" + entr_location + "','" + exit_location + "','" + navi_poiid + "','" + gridcode + "','" + alias + "','" + business_area + "','" + parking_type + "','" + indoor_map + "','" + cpid + "','" + floor + "','" + truefloor + "','" + groupbuy_num + "','" + discount_num + "','" + rating + "','" + cost + "','" + meal_ordering + "','" + seat_ordering + "','" + ticket_ordering + "','" + hotel_ordering + "','" + photos+"');";
             totalSql=totalSql+sql;
-
-            #print(id+" "+tag+" "+name +" "+type +" "+typecode +" "+biz_type +" "+address +" "+location +" "+distance +" "+tel +" "+postcode+" "+ website +" "+email +" "+pcode +" "+pname +" "+citycode +" "+cityname +" "+adcode +" "+adname +" "+entr_location +" "+exit_location +" "+navi_poiid +" "+gridcode +" "+alias +" "+business_area+" "+ parking_type +" "+indoor_map +" "+cpid +" "+floor +" "+truefloor +" "+groupbuy_num +" "+discount_num +" "+rating +" "+cost +" "+meal_ordering +" "+seat_ordering +" "+ticket_ordering +" "+hotel_ordering +" "+titile +" "+url );
-            # print(id+" "+tag+" "+name +" "+type +" "+typecode +" "+biz_type +" "+address +" "+location +" "+distance +" "+tel +" "+postcode+" "
-            #       + website + " " + email + " " + pcode + " " + pname + " " + citycode + " " + cityname + " " + adcode + " " + adname + " " + entr_location + " "
-            #       + exit_location + " " + navi_poiid + " " + gridcode + " " + alias + " "
-            #       + business_area + " "
-            #       + indoor_map + " " + cpid + " "
-            #       + floor + " " + truefloor + " " + groupbuy_num + " " + discount_num
-            #       + " " + rating + " " + cost + " "
-            #       +photos
-            #       );
-
-
-        print(page);
-        print(totalSql);
-        conn = psycopg2.connect(database="mydatabase", user="postgres", password="123456", host="localhost",
-                                port="5432");
+        conn = psycopg2.connect(database="superpower", user="postgres", password="123456", host="localhost",port="5432");
         cur = conn.cursor();
         try:
             cur.execute(totalSql);
@@ -108,13 +93,84 @@ def getGaodePOI(key,rectangle,type,page,offset):
         pages = math.ceil(int(count) / int(offset));
         return pages;
 
-key='a5e9cfffb5d9a00c14142cdc17742a8d';
-rectangle='120.12924,30.30185;120.16018,30.27150';
+
 type=str(150700);
 page=str(1);
 offset="50";
-pages=getGaodePOI(key,rectangle,type,page,offset);
-print(pages);
-if pages>1:
-    for num in range(2, pages+1):
-        getGaodePOI(key, rectangle, type, str(num), offset)
+def getOneRectangleAllPoi(key,rectangle,type,page,offset):
+    pages = getGaodePOI(key, rectangle, type, page, offset);
+    if pages > 1:
+        for num in range(2, pages + 1):
+            getGaodePOI(key, rectangle, type, str(num), offset)
+    return None;
+
+
+# 获取研究区域网格
+def getRegionRectangles():
+    conn = psycopg2.connect(database="superpower", user="postgres", password="123456", host="localhost", port="5432");
+    cur = conn.cursor();
+    cur.execute("select t.__xmin||','||t.ymin||';'||t.__xmax||','||t.ymax rectangle from hangzhou_grid t");
+    rectangleData = cur.fetchall();
+    cur.close();
+    conn.close();
+    return rectangleData;
+
+
+def getUseableKey():
+    currentDate = time.strftime("%Y-%m-%d", time.localtime());
+    conn = psycopg2.connect(database="superpower", user="postgres", password="123456", host="localhost", port="5432");
+    cur = conn.cursor();
+    cur.execute("select KJ.key from (SELECT k.key, COALESCE((select j.poi_reqcount from gaode_keys j where j.date='" + currentDate + "' and j.key=k.key),0) poi_reqcount from (SELECT t.key from gaode_keys t GROUP BY t.key)  K) KJ  where KJ.poi_reqcount<1000");
+    keyData = cur.fetchall();
+    key=keyData[0][0];
+    cur.close;
+    conn.close();
+    if key==None:
+        print("没有可用key了，需要申请更多的账户");
+    return key;
+
+#批量请求研究区域内的一类POI
+def batchGetGaodePoi(type):
+    page = str(1);
+    offset = "50";
+    rectangleData=getRegionRectangles();
+    for rec in rectangleData:
+        print(rec[0]);
+        key=getUseableKey();
+        if key==None:
+            print("没有可用key了，需要申请更多的账户");
+        else:
+            getOneRectangleAllPoi(key, rec[0], type,page,offset);
+        print("一个区域的数据已获取完成");
+
+
+#请求完成后 key计数+1
+def afterRequest(key):
+    currentDate = time.strftime("%Y-%m-%d", time.localtime());
+    conn = psycopg2.connect(database="superpower", user="postgres", password="123456", host="localhost", port="5432");
+    cur = conn.cursor();
+    cur.execute("SELECT count(*) from gaode_keys t where t.key='"+key+"' and t.date='"+currentDate+"'");
+    keyData = cur.fetchall();
+    if keyData[0][0]==0:
+        cur.execute(" INSERT INTO gaode_keys (key, date, poi_reqcount) VALUES('"+key+"','"+currentDate+"'"+",1)");
+    else:
+        cur.execute("UPDATE gaode_keys t set poi_reqcount=t.poi_reqcount+1 where t.key='"+key+"' and t.date='"+currentDate+"'");
+    conn.commit();
+    cur.close;
+    conn.close();
+    print("key :"+key+" +1");
+
+
+# key='a5e9cfffb5d9a00c14142cdc17742a8d';
+# rectangle='120.12924,30.30185;120.16018,30.27150';
+# type=str(150700);
+# page=str(1);
+# offset="50";
+# pages=getGaodePOI(key,rectangle,type,page,offset);
+# print(pages);
+# if pages>1:
+#     for num in range(2, pages+1):
+#         getGaodePOI(key, rectangle, type, str(num), offset)
+
+type=str(150700);
+batchGetGaodePoi(type);
--- sql/触发器.sql
+++ sql/触发器.sql
@@ -7,3 +7,36 @@ return new;
 End	;
 $example_table$ LANGUAGE plpgsql;
 CREATE TRIGGER BeforeInsertInsertGaodePOI_trigger Before INSERT  ON gaode_poi FOR EACH ROW EXECUTE PROCEDURE BeforeInsertGaodePOI ();
+
+
+
+
+--高德数据插入触发器  生成线路
+CREATE OR REPLACE FUNCTION BeforeInsertGaodeBusline() RETURNS TRIGGER AS $example_table$
+declare rec  record;
+declare pointCount  INTEGER;
+declare i  INTEGER;
+declare num INTEGER;
+declare pointLntLngs VARCHAR;
+declare xsArray VARCHAR[];
+declare ysArray VARCHAR[];
+BEGIN
+	--自动进行坐标计算，并赋值
+pointLntLngs:='';
+i:=1;
+num:=1;
+xsArray=string_to_array(New.xs,',','');
+ysArray=string_to_array(New.ys,',','');
+pointCount=array_length(string_to_array(New.xs,',',''),1);
+FOR i IN 1..pointCount loop
+ pointLntLngs=pointLntLngs||xsArray[num]||' '||ysArray[num];
+	if (num!=pointCount) then
+	pointLntLngs=pointLntLngs||',';
+	end if;
+	num=num+1;
+end loop;
+NEW.geom=ST_LineFromText('LINESTRING('||pointLntLngs||')',4326);
+return new;
+end	;
+$example_table$ LANGUAGE plpgsql;
+CREATE TRIGGER BeforeInsertInsertGaodeBusLine_trigger Before INSERT  ON gaode_busline FOR EACH ROW EXECUTE PROCEDURE BeforeInsertGaodeBusline ();
